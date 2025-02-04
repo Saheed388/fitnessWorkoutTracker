41e0ca98f08dfa00743695e00c3b7bb1c7cdb9d6
saheed <saheedjimoh338@gmail.com>
2025-02-04 16:33:57 +0100

    first commit

---
+++ fitnessWorkoutTracker/src/main/java/com/saeed/fitnessWorkoutTracker/implementation/WorkoutNoteServiceImpl.java
@@ -0,0 +1,8 @@
+package com.saeed.fitnessWorkoutTracker.implementation;
+
+import com.saeed.fitnessWorkoutTracker.service.WorkoutNoteService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WorkoutNoteServiceImpl implements WorkoutNoteService {
+}
---
+++ fitnessWorkoutTracker/src/main/java/com/saeed/fitnessWorkoutTracker/implementation/WorkoutReportServiceImpl.java
@@ -0,0 +1,8 @@
+package com.saeed.fitnessWorkoutTracker.implementation;
+
+import com.saeed.fitnessWorkoutTracker.service.WorkoutReportService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WorkoutReportServiceImpl implements WorkoutReportService {
+}
